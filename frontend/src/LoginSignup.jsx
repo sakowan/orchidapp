9e0ec31d32f4835305d89eb73b1ca66e8022743d
Bella <bella@Bellas-Laptop.local>
2024-07-26 13:03:21 +0900

    updated name of variable and function for Login component

--- frontend/src/LoginSignup.jsx
+++ frontend/src/LoginSignup.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const LoginSignup = () => {
-    const [products, setProducts] = useState([]);
+    const [bamuser, setBamsuser] = useState([]);
     
       return (
         <div>
