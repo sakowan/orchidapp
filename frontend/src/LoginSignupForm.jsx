052e8c5beefb5a880f11fae101d70cb9f85710a9
Bella <bella@Bellas-Laptop.local>
2024-08-12 14:43:45 +0900

    before user auth FE changes

--- frontend/src/LoginSignupForm.jsx
+++ frontend/src/LoginSignupForm.jsx
@@ -2,10 +2,14 @@ import './index.css';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const LoginSignupForm = ({client, onLoginUser, currentUser}) => {
+const LoginSignupForm = ({onLoginUser, currentUser}) => {
   const navigate = useNavigate();
   const [isRegister, setIsRegister] = useState(false);
   const [data, setData] = useState({first_name: "", last_name: "", phone_number: "", email: "", password: ""});
+
+  useEffect(() => {
+    console.log('Current user signup', currentUser)
+  }, []);
 
   const handleRegisterToggle = () => {
     setIsRegister(!isRegister);
@@ -19,37 +23,12 @@ const LoginSignupForm = ({client, onLoginUser, currentUser}) => {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const userData = {
-      first_name: data.first_name,
-      last_name: data.last_name,
-      phone: data.phone_number,
-      email: data.email,
-      password: data.password
-    };
-    axios
-      .post("http://127.0.0.1:8000/api/users/", userData)
-      .then((response) => {
-        console.log(response);
-        navigate('/');
-      })
-    })
+  const handleRegister = async (e) => {
+    console.log('handleRegister')
   };
 
   const handleLogin = async (e) => {
-    e.preventDefault();
-    client.post(
-      formUrl,
-      {
-        email: data.email,
-        password: data.password
-      }
-    ).then(function(result){
-      onLoginUser(true);
-      console.log('login token', client)
-      navigate('/');
-    })
+    console.log('handleLogin')
   }
     
   return (
@@ -62,11 +41,11 @@ const LoginSignupForm = ({client, onLoginUser, currentUser}) => {
       </div>
       
       {/* Right Side */}
-      <div className="flex w-2/5 bg-gray-100 bg-opacity-75 p-12 fixed right-0 h-full">
+      <div className="login-parent-sm">
         {/* Content here will be static */}
-        <form onSubmit={isRegister ? handleRegister : handleLogin}>
+        <form onSubmit={handleLogin}>
           <div className='w-full p-12 space-y-4'>
-            <h1 className="mt-14 text-2xl font-bold text-gray text-center">{isRegister ? `Register` : `Sign in`}</h1>
+            <h1 className="mt-14 pb-4 text-2xl font-bold text-gray-600">{isRegister ? `Register` : `Sign in`}</h1>
             {isRegister &&
             <>
               <input type="text" 
