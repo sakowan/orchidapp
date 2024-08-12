3797477ff167ef8974b5cee6514f3ead0d9606b9
Bella <bella@Bellas-Laptop.local>
2024-08-12 15:34:06 +0900

    login&register produces sessionid&csrf, logout doesn't remove either

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,28 @@
-// Components can have dedicated stylesheets. Eg. for App.jsx component, it would be App.css
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
-import ProductListing from "./ProductListing"
-import LoginSignupForm from "./LoginSignupForm"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Components
 import Navbar from "./Navbar"
 import Checkout from "./Checkout"
 import Landing from "./Landing"
+import ProductListing from "./ProductListing"
+import LoginSignupForm from "./LoginSignupForm"
+
+// Axios
+import axios from 'axios';
+axios.defaults.xsrfCookieName= 'csrftoken';
+axios.defaults.xsrfHeaderName= 'X-CSRFToken';
+axios.defaults.withCredentials= true;
+const client = axios.create({
+    baseURL: "http://127.0.0.1:8000"
+});
 
 function App() {
-    const [currentUser, setCurrentUser] = useState(false);
-    const [csrfToken, setCsrfToken] = useState();
-    
-    const loginUser = (u) => {
-        setCurrentUser(true);
-        console.log("Current user APP:", currentUser);
-    }
-
     return (
         <Router>
             <Navbar/>
             <Routes>
-                <Route path='/login' element={<LoginSignupForm onLoginUser={loginUser} currentUser={currentUser}/>}></Route>
+                <Route path='/login' element={<LoginSignupForm client={client}/>}></Route>
                 <Route path='/' element={<Landing/>}></Route>
                 <Route path='/product_listings' element={<ProductListing/>}></Route>
                 <Route path='/checkout' element={<Checkout/>}></Route>
