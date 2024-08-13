64e9ff15a4d11888c805e827dc3caeb984e9c8f9
Bella <bella@Bellas-Laptop.local>
2024-08-13 16:20:37 +0900

    Made FE api interceptor with tokens, made ProtectedRoute component

---
+++ frontend/src/constants.js
@@ -0,0 +1,4 @@
+//Why: Use local storage to store access&refresh tokens in the browser, and have a key that I can use to access in my local storage
+// These are keys used to access access&refresh tokens 
+export const ACCESS_TOKEN = "access";
+export const REFRESH_TOKEN = "refresh";
