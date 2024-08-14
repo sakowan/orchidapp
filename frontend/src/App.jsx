7c7030a18dcaea382880edf5c3a998fd0fcc10c8
Bella <bella@Bellas-Laptop.local>
2024-08-14 14:14:05 +0900

    user can register and access&refresh tokens stored in local storage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,21 +9,12 @@ import ProductListing from "./ProductListing"
 import LoginSignupForm from "./LoginSignupForm"
 import ProtectedRoute from './ProtectedRoute';
 
-// Axios
-import axios from 'axios';
-axios.defaults.xsrfCookieName= 'csrftoken';
-axios.defaults.xsrfHeaderName= 'X-CSRFToken';
-axios.defaults.withCredentials= true;
-const client = axios.create({
-    baseURL: "http://127.0.0.1:8000"
-});
-
 function App() {
     return (
         <Router>
             <Navbar/>
             <Routes>
-                <Route path='/login' element={<LoginSignupForm client={client}/>}></Route>
+                <Route path='/login' element={<LoginSignupForm/>}></Route>
                 <Route path='/' element={<Landing/>}></Route>
                 <Route path='/product_listings' element={<ProductListing/>}></Route>
                 <Route path='/checkout' element={
