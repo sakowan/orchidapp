3e5c6a8e1304ef77b74fedd54ee701aba0b72cc8
Bella <bella@Bellas-Laptop.local>
2024-08-19 18:08:39 +0900

    doing star for reviews and updated images for products

--- frontend/src/DropdownUser.jsx
+++ frontend/src/DropdownUser.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { UserContext } from './UserContext';
 
 const DropdownUser = () => {
@@ -8,9 +8,15 @@ const DropdownUser = () => {
     const navigate = useNavigate();
 
     function submitLogout(e) {
+        console.log('logging out')
         localStorage.clear();
         navigate(0);
       }
+    
+    function goLogin(e) {
+        console.log('Redirecting to login')
+        navigate("/login");
+    }
     return (
         <div className='absolute mt-8 z-20 w-[14rem] right-[5rem] bg-white border border-gray-200 rounded-sm text-base text-gray-600 ibm-plex-mono-light'>
             <ul>
@@ -20,10 +26,10 @@ const DropdownUser = () => {
                     <li className="flex items-center px-2 h-10 hover:bg-gray-100">Account</li>
                     <li className="flex items-center px-2 h-10 hover:bg-gray-100">Orders</li>
                     <hr/>
-                    <li className="flex items-center px-2 h-10 hover:bg-gray-100"><button onClick={submitLogout}>Logout</button></li>
+                    <li className="flex items-center px-2 h-10 hover:bg-gray-100"><button className= "w-full text-left" onClick={submitLogout}>Logout</button></li>
                 </>
                 :
-                    <li className="flex items-center px-2 h-10 hover:bg-gray-100"><button onClick={submitLogout}>Sign in</button></li>}
+                    <li className="flex items-center px-2 h-10 hover:bg-gray-100"><button className= "w-full text-left" onClick={goLogin}>Sign in</button></li>}
             </ul>
         </div>
     )
---
+++ frontend/src/ProductView.jsx
@@ -0,0 +1,74 @@
+import api from "./api"
+import React from 'react';
+import MainBody from './MainBody'
+
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Rating, ThinStar } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+
+const starStyling = {
+  itemShapes: ThinStar,
+  activeFillColor: '#8996e3', //colour-5
+  inactiveFillColor: '#D1D5DB' //gray-300
+}
+
+const ProductView = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [product, setProduct] = useState(location.state?.product || {});
+
+  const getProduct = async () => {
+    try{
+      const response = await api.get(location.pathname)
+      if (response.data.id != null) {
+        setProduct(response.data)
+      }
+    } catch (e) {
+      console.log('Error fetching product listings:', e)
+      navigate("/product_listings")
+    }
+  };
+  
+  useEffect(() => {
+    getProduct();
+  }, [product])
+
+  return (
+    <MainBody>
+      {/* First Row */}
+      <div className="flex w-full h-full p-5">
+        {/* Left Side */}
+        <div className="w-1/2 mr-5">
+          <img 
+          className="rounded-lg h-full border border-gray-100"
+          src={`/src/assets/images/${product.img_url}`} alt="product"/>
+
+        </div>
+
+        {/* Right Side */}
+        <div className="w-1/2 p-6 bg-gray-100 rounded-lg text-gray-500">
+          <h1 className="h1-product-view">{product.name}</h1>
+          
+          {/* Review indicator */}
+          <div className="flex">
+            <span className="pt-2 w-4/5">{product.desc_brief}</span>
+            <Rating className="w-1/5" readOnly value={3.5} itemStyles={starStyling}/>
+          </div>
+          <div className="my-10">{product.desc_long}</div>
+          <hr/>
+          <button className="main-button-product-view">ADD TO CART</button>
+        </div>
+      </div>
+
+      {/* Reviews */}
+      <div
+      className="w-full bg-gray-100 rounded-lg m-5 p-5">
+        <h1 className="h1-product-view">Reviews</h1>
+      </div>
+
+    </MainBody>
+  );
+};
+
+export default ProductView;
