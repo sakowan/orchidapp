ea29415c16c3e37560525c46cd7802c4e0066a4c
Bella <bella@Bellas-Laptop.local>
2024-08-29 18:51:36 +0900

    reformatted homepage top banner

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -8,40 +8,27 @@ const Home = () => {
     setNewProds(response.data)
   }
   return (
-    <>
-      <div className='h-screen top-[1rem] relative flex items-center justify-center'>
-        {/* Background Container */}
-        <div className="absolute inset-0 flex items-center justify-center z-0">
-          <div className="relative w-[98%] h-[75%] bg-[url('/src/assets/images/stock/bg4.jpg')] opacity-30 bg-cover bg-center animate-zoomOut rounded-lg">
-            {/* Background image */}
-          </div>
-        </div>
-
-        {/* Foreground Content */}
-        <div className="absolute inset-0 z-10 flex items-center">
-          <div className="pl-10 flex flex-col items-center text-center w-1/2">
-            <h1 className="home-phrase">
-              Introducing Orchid
-            </h1>
-            <h2 className='text-colour-7 animate-typewriter'>
+    <div className="w-screen h-screen flex flex-col">
+      <div className='flex justify-center mx-auto relative top-[8rem] w-[98%] h-3/4 bg-white animate-zoomOut'>
+        <div id="banner1" className="flex justify-center w-full h-full bg-[url('/src/assets/images/bg4_transparent.png')] bg-cover bg-center rounded-lg">
+          <div className='w-1/2 h-full flex flex-col items-center justify-center'>
+            <h1 className="home-h1">Introducing Orchid</h1>
+            <h2 className='text-colour-7'>
               Explore our collection for a subtle blend of feminine grace and soft allure.
             </h2>
             <button className='my-3 w-1/4 h-[3rem] text-center text-white text-lg rounded-lg bg-colour-3 animate-pulse'>Shop Now</button>
           </div>
-          <div className='h-full w-1/2 flex items-center justify-center'>
-            <img className="opacity-70 h-[40rem] animate-zoomOut" src="/src/assets/images/stock/girl3.jpg"/>
-            {/* <img className="h-[40rem]" src="/src/assets/images/stock/girl4.jpg"/> */}
-
+          <div className='w-1/2 h-full flex justify-center'>
+            <img className="opacity-70 h-full" src="/src/assets/images/stock/girl3.jpg" alt="Girl" />
           </div>
         </div>
       </div>
-      <div className='px-4 bg-white w-full h-full flex items-center justify-center'>
-        <h1 className="home-phrase !text-colour-7">
-          What's New
-        </h1>
 
+      <div id="banner2" className="flex justify-center w-[98%] h-3/4 mx-auto mt-[10rem] border border-gray-400">
+      <h1 className="home-h1">What's New</h1>
+ 
       </div>
-    </>
+    </div>
   )
 }
 
