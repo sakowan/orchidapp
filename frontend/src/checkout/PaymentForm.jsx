27cd89c84cde24f376176a5f52b4adaa6ac24d94
Bella <bella@Bellas-Laptop.local>
2024-09-01 17:29:36 +0900

    create method working and sending to Stripe dashboard

--- frontend/src/checkout/PaymentForm.jsx
+++ frontend/src/checkout/PaymentForm.jsx
@@ -1,12 +1,13 @@
+import { ApiService } from '../api';
 import React, { useState, useEffect } from 'react';
 import { ChevronLeft } from 'lucide-react';
-import {useElements, useStripe, CardNumberElement, CardExpiryElement, CardCvcElement} from "@stripe/react-stripe-js";
+import {useElements, useStripe, CardElement, CardNumberElement, CardExpiryElement, CardCvcElement} from "@stripe/react-stripe-js";
 
 const stripeStyles = {
     style: {
       base: {
         fontSize: '1.25rem', // Equivalent to `text-lg` in Tailwind CSS
-        lineHeight: '2.75rem',
+        lineHeight: '3rem',
         '::placeholder': {
           color: '#9CA3AF', // gray-400
         },
@@ -28,8 +29,36 @@ const PaymentForm = ({ formData }) => {
         }
     }
 
-    const handleSubmit = async () => {
-        console.log('hi')
+    const handleSubmit = async (e) => {
+        console.log('Submit payment.')
+        e.preventDefault();
+
+        // Check if Stripe.js has loaded yet
+        if (!stripe || !elements) {
+            return;
+        }
+
+        // Retrieve card
+        const card = elements.getElement(CardNumberElement);
+        if (card == null) {
+            console.log('Card element does not exist.')
+            return;
+        }
+        
+        // Create a payment method
+        // When passing card, exp and cvc element info is also passed automatically.
+        const {paymentMethod, error} = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+        ApiService.saveStripeInfo({
+            email, payment_method_id: paymentMethod.id})
+          .then(response => {
+            console.log(response.data);
+          }).catch(error => {
+            console.log(error)
+        });
     };
 
     return (
@@ -38,12 +67,20 @@ const PaymentForm = ({ formData }) => {
             <p>All transactions are secure and encrypted.</p>
             <div className="flex flex-col justify-between p-8 relative border border-gray-400 w-4/5 rounded-md bg-gray-100">
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor='card-num'>Card Number</label>
+                    <div className="flex justify-between">
+                        <label htmlFor='card-num'>Card Number</label>
+                        {/* Card Symbols*/}
+                        <div className='flex items-center'>
+                            <img src='/src/assets/images/banks/ma_1.png' className='h-[1.75rem]'/>
+                            <img src='/src/assets/images/banks/visa_blue.png' className='h-[1rem]'/>
+                            <img src='/src/assets/images/banks/JCB.svg' className='ml-2 h-[1.5rem]'/>
+                            <img src='/src/assets/images/banks/amex.png' className='ml-2 h-[2rem]'/>
+                        </div>
+                    </div>
                     <CardNumberElement id='card-num'
                     className="stripe-input w-full"
                     placeholder="Card Number"
-                    options={stripeStyles}>
-                    </CardNumberElement>
+                    options={stripeStyles}/>
                     
                     <div className="flex mt-4 space-x-8">
                         <div className="flex-col w-1/2">
@@ -51,8 +88,7 @@ const PaymentForm = ({ formData }) => {
                             <CardExpiryElement
                             id='card-exp'
                             className="stripe-input"
-                            options={stripeStyles}>
-                            </CardExpiryElement>
+                            options={stripeStyles}/>
                         </div>
 
                         <div className="flex-col w-1/2">
@@ -60,17 +96,16 @@ const PaymentForm = ({ formData }) => {
                             <CardCvcElement
                             id='card-cvc'
                             className="stripe-input"
-                            options={{... stripeStyles, placeholder: '789'}}>
-                            </CardCvcElement>
+                            options={{... stripeStyles, placeholder: '789'}}/>
                         </div>
                     </div>
                 </form>
                 <div className="flex items-center justify-between mt-8">
-                    <div className='flex'>
+                    <div className='flex cursor-pointer'>
                         <ChevronLeft strokeWidth={1}/>
-                        <a>Return to shipping</a>
+                        <a className="underline">Return to shipping</a>
                     </div>
-                    <button onClick={handleSubmit} className='btn-1 btn-1-hover'>Pay now</button>
+                    <button onClick={(e) => handleSubmit(e)} className='btn-1 btn-1-hover'>Pay now</button>
                 </div>
             </div>
         </>
