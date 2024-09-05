4b6ed19b9baf4b3cbeee852b988d1ea3cf15bed3
Bella <bella@Bellas-Laptop.local>
2024-09-05 16:05:28 +0900

    moved drawer to individual component

--- frontend/src/ProductView.jsx
+++ frontend/src/ProductView.jsx
@@ -7,6 +7,7 @@ import axios from 'axios'
 import { UserContext } from './UserContext';
 import MainBody from './MainBody'
 import { CartContext } from './CartContext';
+import CartDrawer from './CartDrawer';
 
 // STYLE IMPORTS
 import { Rating } from '@smastrom/react-rating'
@@ -168,6 +169,8 @@ const ProductView = () => {
 
   useEffect(() => { //User & cart
     console.log('Function: useEffect')
+    const pathRegex = /^\/products\/.*/
+    console.log('location', pathRegex.test(location.pathname))
     if (user) {
       if(user.cart_products.length > 0) {
         setCartProds(user.cart_products)
@@ -191,57 +194,12 @@ const ProductView = () => {
   return (
     <MainBody>
       {/* Drawer */}
-      {cartProds && 
-        <ThemeProvider value={drawerTheme}>
-          <Drawer placement="right" open={open} onClose={closeDrawer} className={open ? '!w-2/5 !max-w-none' : ''}>
-            <div className='p-6'>
-              <h1 className='pv-h1 text-center pb-6'>ITEMS</h1>
-              <hr className='pv-hr'/>
-              {cartProds.map((p, index) => (
-                <div key={p.id} className="my-2">
-                  <div className='flex py-2'>
-                    <img src={`/src/assets/images/${p.product_info.main_img}`} className="w-[6rem] h-[6rem] mr-4 border border-gray-100 rounded-sm" alt={p.product_info.name}/>
-                    <div className="flex flex-col justify-between w-full">
-                      <div className="flex justify-between w-full">
-                        <h3 className="pv-h3 w-4/5">{p.product_info.name}</h3>
-                        <h3 className="pv-h3 text-right w-1/5">¥{(p.product_info.price * p.quantity).toFixed(2)}</h3>
-                      </div>
-
-                      <div className="flex justify-between">
-                        <div className="flex max-w-[10rem]">
-                          <button onClick={() => adjustQty(index, p.product, p.quantity, false)} type="button" className="rounded-s-lg pv-qty-btn">
-                            <Minus className='lucide-icon'/>
-                          </button>
-                          <input
-                            type="text"
-                            data-input-counter
-                            data-input-counter-min="1"
-                            className="bg-gray-50 border-x-0 border-gray-300 h-10 text-center text-gray-900 text-sm block w-full py-2.5"
-                            placeholder="1"
-                            value={p.quantity}
-                            onChange={updateProductBackend}
-                            required
-                          />
-                          <button onClick={() => adjustQty(index, p.product, p.quantity, true)}type="button" className="rounded-e-lg pv-qty-btn">
-                            <Plus className='lucide-icon'/>
-                          </button>
-                        </div>
-                        <div className="flex text-left">
-                          <button
-                          onClick={() => removeCartProduct(index, p)}  
-                          className='pv-rm-btn'>Remove</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <hr className='pv-hr'/>
-                </div>
-              ))}
-            </div>
-          </Drawer>
-        </ThemeProvider>
-      }
-
+      <CartDrawer
+        open={open}
+        closeDrawer={closeDrawer}
+        adjustQty={adjustQty}
+        removeCartProduct={removeCartProduct}
+      />
 
       {/* First Row */}
       <div className="flex w-full p-5">
