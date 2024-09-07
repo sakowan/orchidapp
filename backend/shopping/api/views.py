a6b87f3e592092956d0ccfca2a5f5a41dce85a1b
Bella <bella@Bellas-Laptop.local>
2024-09-07 16:05:39 +0900

    Passed updated num_items back when calling the CartProduct post api

--- backend/shopping/api/views.py
+++ backend/shopping/api/views.py
@@ -91,19 +91,24 @@ class CartProductViewSet(ModelViewSet):
         product_id = request.data.get('product_id')
         quantity = request.data.get('quantity')
         cp = CartProduct.objects.filter(cart_id=cart_id, product_id=product_id).first()
-        queryset = request.user.cart_products
+        user = BamUser.objects.get(email=request.user)
+        
 
         # If cp already exists
         if (cp):
             cp.quantity = quantity
             cp.save()
-            return Response({'message': 'CartProduct updated', "cart_products": CartProductSerializer(queryset, many=True).data}, status=status.HTTP_200_OK)
+            cart_products = CartProduct.objects.filter(user_id = user.id)
+            num_items = cart_products.aggregate(total=Sum('quantity'))['total']
+            return Response({'message': 'CartProduct updated', "cart_products": CartProductSerializer(cart_products, many=True).data, "num_items": num_items}, status=status.HTTP_200_OK)
 
         else:
             cp = CartProduct(cart_id=cart_id, product_id=product_id, quantity=quantity)
             cp.user = request.user
             cp.save()
-            return Response({'message': 'CartProduct created', "cart_products": CartProductSerializer(queryset, many=True).data}, status=status.HTTP_201_CREATED)
+            cart_products = CartProduct.objects.filter(user_id = user.id)
+            num_items = cart_products.aggregate(total=Sum('quantity'))['total']
+            return Response({'message': 'CartProduct created', "cart_products": CartProductSerializer(cart_products, many=True).data, "num_items": num_items}, status=status.HTTP_201_CREATED)
 
     def list(self, request):
         queryset = request.user.cart_products.all().order_by('created_at')
