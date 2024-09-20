81ef14d8e16dfac3314994fe8f0bbfec30de362f
Bella <bella@Bellas-Laptop.local>
2024-09-20 15:49:31 +0900

    fruits upload to the path i specify complains/ID in s3 bucket

--- backend/shopping/helpers.py
+++ backend/shopping/helpers.py
@@ -4,3 +4,6 @@ import string
 def gen_random_string(length=12):
     """Generates a random string of letters and digits with the specified length."""
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+def complaint_img_upload_path(instance, filename):
+    return f'complaints/{instance.complaint_order_product.complaint.id}/{filename}'
