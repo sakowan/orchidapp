e77ce8622ed833cf62fb208df827dadd98c1f4b7
Bella <bella@Bellas-Laptop.local>
2024-09-29 16:39:25 +0900

    carousel can rotate through elems

--- frontend/src/products/ProductCarousel.jsx
+++ frontend/src/products/ProductCarousel.jsx
@@ -5,33 +5,47 @@ import React, { useState, useEffect } from 'react';
 import ProductCard from './ProductCard';
 
 // Design
+import { Carousel } from 'flowbite';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { carousel } from '@material-tailwind/react';
 
 const ProductCarousel = () => {
   const [products, setProducts] = useState([]);
-  const [currentCar, setCurrentCar] = useState(0)
+  const [items, setItems] = useState([]);
+  const $prevButton = document.getElementById('data-carousel-prev');
+  const $nextButton = document.getElementById('data-carousel-next');
 
-  const prevCar = () => {
-    let nextCar = currentCar-1
-    if(currentCar == 0){
-      nextCar = 3
-    }
-    setCurrentCar(nextCar)
-    document.getElementById(`car_${currentCar}`).classList.add('transform', '-translate-x-[100%]')
-    document.getElementById(`car_${nextCar}`).classList.remove('hidden')
-  }
+  const createCarouselDivs = (chunk, x) => {
+    console.log("Function: createCarouselDivs");
 
-  const nextCar = () => {
-    let nextCar = currentCar+1
-    if(currentCar == 3){
-      nextCar = 0
+    return (
+      <div key={`chunk_${x}`}>
+        {chunk.map((prod, y) => (
+          <ProductCard key={`prod_${y}`} product={prod} />
+        ))}
+      </div>
+    );
+  };
+
+  const initCarouselItems = () => {
+    console.log("Function: initCarouselItems");
+
+    const tempItems = [];
+    for (let i = 0; i < products.length; i++) {
+      const item = createCarouselDivs(products[i], i);
+      let obj = {
+        position: i,
+        el: item
+      }
+      
+      tempItems.push(obj);
     }
-    setCurrentCar(nextCar)
-    document.getElementById(`car_${currentCar}`).classList.add('hidden')
-    document.getElementById(`car_${nextCar}`).classList.remove('hidden')
-  }
+    setItems(tempItems);
+  };
 
   useEffect(() => {
+    console.log("useEffect for fetchProducts");
+
     // Fetch product data once when the component loads
     const fetchProducts = async () => {
       try {
@@ -47,40 +61,90 @@ const ProductCarousel = () => {
   }, []);
 
   useEffect(() => {
-    let currentCar = document.getElementById("car_0")
-    for(let i=1; i<products.length; i++){
-      let hideCar = document.getElementById(`car_${i}`)
-      hideCar.classList.add('hidden')
+    if (products.length > 0) {
+      console.log("useEffect for Products");
+      initCarouselItems();
+      // console.log('products', products[0])
     }
-    console.log(currentCar)
+  }, [products]);
 
-  }, [products])
+  useEffect(() => {
+    if (items.length > 0 && products.length > 0) {
+      const carouselElement = document.getElementById('carousel-example');
+      const items = [
+          {
+              position: 0,
+              el: document.getElementById('carousel-item-1'),
+          },
+          {
+              position: 1,
+              el: document.getElementById('carousel-item-2'),
+          },
+          {
+              position: 2,
+              el: document.getElementById('carousel-item-3'),
+          },
+          {
+              position: 3,
+              el: document.getElementById('carousel-item-4'),
+          },
+      ];
+      const carousel = new Carousel(carouselElement, items);
+      $prevButton.addEventListener('click', () => {
+        carousel.prev();
+      });
+    
+      $nextButton.addEventListener('click', () => {
+          carousel.next();
+      });
+    }
+  }, [items]);
 
   return (
-    <div className="flex flex-col bg-my-muted w-full p-8 justify-center items-center">
-      <h1 className="text-[36px] mb-4 text-gray-700 rubik-80s-fade-regular">Best Sellers</h1>
-      
-      <div id="controls-carousel" className="relative w-full" data-carousel="static">
-        {products.map((chunk, index) => (
-          <div id={`car_${index}`} key={index} className="flex w-full items-center justify-center duration-700 ease-in-out" data-carousel-item>
-            {chunk.map((p) => (
-              <ProductCard key={p.id} product={p} className="!w-[10rem]" />
+    <div id="parent" className="p-6 bg-gray-50">
+      <h2 className="text-[36px] text-colour-4 rubik-80s-fade-regular text-center">Bestsellers</h2>
+      <div id="carousel-example" className="w-full relative h-[44rem] overflow-hidden">
+        {products.length > 0 && 
+        <div>
+          <div id="carousel-item-1" className="flex w-full hidden duration-700 ease-in-out">
+            {products[0].map(product => (
+              <div>
+                <ProductCard product={product}/>
+              </div>
             ))}
           </div>
-        ))}
-
-        <button onClick={prevCar} type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-colour-2/30 group-hover:bg-colour-4/50 group-focus:outline-none">
-            <ChevronLeft className="text-white" />
-          </span>
+          <div id="carousel-item-2" className="flex w-full hidden duration-700 ease-in-out">
+            {products[1].map(product => (
+              <div>
+                <ProductCard product={product}/>
+              </div>
+            ))}
+          </div>
+          <div id="carousel-item-3" className="flex w-full hidden duration-700 ease-in-out">
+            {products[2].map(product => (
+              <div>
+                <ProductCard product={product}/>
+              </div>
+            ))}
+          </div>
+          <div id="carousel-item-4" className="flex w-full hidden duration-700 ease-in-out">
+            {products[3].map(product => (
+              <div>
+                <ProductCard product={product}/>
+              </div>
+            ))}
+          </div>
+        </div>
+        }
+        {/* Carousel Navigation Controls */}
+        <button id="data-carousel-prev" className="absolute z-[999] left-0 top-[50%] bg-gray-100 rounded-3xl w-10 h-10 flex items-center justify-center border border-colour-4 border-2"> 
+          <ChevronLeft />
         </button>
-        
-        <button onClick={nextCar} type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-colour-2/30 group-hover:bg-colour-4/50 group-focus:outline-none">
-            <ChevronRight className="text-white" />
-          </span>
+        <button id="data-carousel-next" className="absolute z-[999] right-0 top-[50%] bg-gray-100 rounded-3xl w-10 h-10 flex items-center justify-center border border-colour-4 border-2">
+          <ChevronRight />
         </button>
       </div>
+
     </div>
   );
 };
